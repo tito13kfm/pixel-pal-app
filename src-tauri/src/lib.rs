ec4011a3d7b613e66f4459870103aba4cc24e7a3
tito13kfm <tito13kfm@gmail.com>
2026-05-27 14:03:29 -0400

    rust: register dialog + fs plugins in Tauri builder

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,6 +18,8 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .plugin(
             tauri_plugin_log::Builder::default()
                 .targets([
